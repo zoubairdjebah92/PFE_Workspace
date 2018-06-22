b7c3adcfba709731e5b3ee539f8496139c47cab2
zoubairdjebah92 <djbhzoubair@gmail.com>
2018-06-22 21:46:53 +0200

    1

---
+++ e-Learning/src/main/java/pfe/elearning/ELearningApplication.java
@@ -0,0 +1,12 @@
+package pfe.elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningApplication.class, args);
+	}
+}
